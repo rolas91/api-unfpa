b6031052eb94aa5ad9cab6c87800798e444b46b6
Rolando Sanchez <rsanchez@numu.digital>
2020-11-27 14:23:57 -0600

    impro login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -75,7 +75,7 @@ export default (app: Application): void => {
       });
     } catch (error) {
       console.log(error);
-      res.status(200).send({ message: error });
+      res.status(200).send({ message: error.message });
     }
   });
   // create a new jwt token for an especific user by Id
