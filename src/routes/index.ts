2c07585b489e58a5d6d71116e3c61dcfab9b9f3a
Rolando Sanchez <rsanchez@numu.digital>
2020-12-29 13:02:22 -0600

    cambio a post get users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -124,7 +124,7 @@ export default (app: Application): void => {
   app.post('/api/v1/user/getusers', async (req, res) => {
     try {
       const response = await user.getUsers();
-      res.status(200).send(response);
+      res.status(200).json(response);
     } catch (error) {
       res.status(500).send({ message: error.message });
     }
