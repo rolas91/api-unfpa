76aee7a8eae5d0180295935659a91a1e7cd3dec8
Rolando Sanchez <rsanchez@numu.digital>
2021-01-19 22:50:27 -0600

    agrego data al enviar notificacion por firebase

--- src/controllers/appointments.ts
+++ src/controllers/appointments.ts
@@ -153,6 +153,7 @@ const executeReminder24horas = async() =>{
                 'title': 'Recuerda Tu Cita Médica',
                 'text': `Tiene una Cita Médica mañana ${dataAppointment.date} a las ${dataAppointment.hour}.`
             },
+            "priority": "high",
             "tokens":fcm_tokens
         };
                 
