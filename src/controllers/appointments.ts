028e9b883925943f91df7eff84850496516faba0
Rolando Sanchez <rsanchez@numu.digital>
2021-02-03 21:13:53 -0600

    cambio las notificaciones

--- src/controllers/appointments.ts
+++ src/controllers/appointments.ts
@@ -111,7 +111,7 @@ const executeReminderForDay = async() =>{
 
         var notification = {
             'title': 'Recuerda Tu Cita Médica',
-            'text': `Tiene una Cita dentro de una hora, exactamente el ${dataAppointment.date} a las ${dataAppointment.hour}.Favor estar pendiente dentro de la app al menos 5 min antes de la cita`
+            'text': `Tiene una Cita dentro de una hora, exactamente el ${dataAppointment.date} a las ${dataAppointment.hour}.Favor estar pendiente dentro de la app al menos 10 min antes de la cita`
           };
         
           var notification_body = {
@@ -217,7 +217,7 @@ const executeReminder24horas = async() =>{
 
         var notification = {
             'title': 'Recuerda Tu Cita Médica',
-            'text': `Tiene una Cita Médica mañana ${dataAppointment.date} a las ${dataAppointment.hour}.Favor estar pendiente dentro de la app al menos 5 min antes de la cita`
+            'text': `Tiene una Cita Médica mañana ${dataAppointment.date} a las ${dataAppointment.hour}.Favor estar pendiente dentro de la app al menos 10 min antes de la cita`
           };
         
           var notification_body = {
