64cfd7174c68ec560a7871c9386a36b1303f1106
Rolando Sanchez <rsanchez@numu.digital>
2021-02-04 01:00:08 -0600

    vuelvo a poner como estaba la query

--- src/controllers/appointments.ts
+++ src/controllers/appointments.ts
@@ -385,7 +385,8 @@ const getAppointmentByHour = async (doctorId:any, today:Date, hour:string) => {
     .leftJoinAndSelect("patient.user", "user")
     .where("appointment.doctorId = :doctorId", {doctorId:doctorId})
     .andWhere("appointment.date = :today", {today:today})
-    .andWhere("ADDTIME(appointment.hour,'00:15:00') >= :hour", {hour:hour})
+    .andWhere("appointment.hour >= :hour", {hour:hour})
+    // .andWhere("ADDTIME(appointment.hour,'00:15:00') >= :hour", {hour:hour})
     // .andWhere(`appointment.hour BETWEEN '${hour}' AND  ADDTIME (appointment.hour, '00: 10: 00')`)
     // .addSelect('appointment.gestationWeeksDate as mierda')
     
