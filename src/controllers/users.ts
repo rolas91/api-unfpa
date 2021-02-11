10439b9295915c5adaf5ecc6b8ddf6b262edbd34
Rolando Sanchez <rsanchez@numu.digital>
2021-02-10 19:10:08 -0600

    modifico getUserLike 8

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -55,8 +55,9 @@ const getUserLike = async (data:{
     LIKE '${params}%' OR u.email LIKE '${params}%' OR u.phone LIKE '${params}%' OR u.cedula LIKE '${params}%') and pu.userId = ${doctorId}`)
   }
   else{   
-    return await entityManager.query(`SELECT * FROM user  WHERE (concat(firstname,' ',lastname) LIKE '${params}%' OR 
-      email LIKE '${params}%' OR phone LIKE '${params}%' OR cedula LIKE '${params}%') and typeUser = 'paciente' `)
+    return await entityManager.query(`SELECT * FROM user u 
+    inner join patient p on u.id = p.userId inner join patient_doctors_user pu on p.id = pu.patientId WHERE (concat(u.firstname,' ',u.lastname) 
+    LIKE '${params}%' OR u.email LIKE '${params}%' OR u.phone LIKE '${params}%' OR u.cedula LIKE '${params}%')`)
   }
   
 };
