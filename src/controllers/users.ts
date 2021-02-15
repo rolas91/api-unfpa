6e6dcb98e58e4e74178df854dfe0c371492e90c7
Rolando Sanchez <rsanchez@numu.digital>
2021-02-15 04:49:55 -0600

    agrego actualizacion a mensajes leidos

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -109,9 +109,8 @@ const getMessages = async(data:{
   .set({ read: true })
   .where("receiverId = :receive", { receive: receive })
   .andWhere("state = 'r'")
-  .orWhere("senderId = :sender", { sender: sender })
-  .andWhere("state = 's'")
   .execute();
+
   
   return await getRepository(Message).createQueryBuilder("message")
               .leftJoinAndSelect("message.sender", "sender")
