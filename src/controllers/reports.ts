6132fed8c90c5e4c1c6e950abeee8da4c5c42c62
Rolando Sanchez <rsanchez@numu.digital>
2021-02-24 14:19:41 -0600

    corrijo la edad del paciente en el reporte

--- src/controllers/reports.ts
+++ src/controllers/reports.ts
@@ -6,7 +6,7 @@ const report = async():Promise<any> =>{
     try {        
         const entityManager = getManager();
         const responseQuery = entityManager.query(`
-            SELECT u.id AS Código, u.firstname AS Nombre, u.lastname AS Apellido, u.cedula AS NoCédula, YEAR(CURDATE())-YEAR(u.birth) AS Edad, ap.date AS FechaConsulta, 
+            SELECT u.id AS Código, u.firstname AS Nombre, u.lastname AS Apellido, u.cedula AS NoCédula, YEAR(CURDATE())-YEAR(u.birth) + IF(DATE_FORMAT(CURDATE(),'%m-%d') > DATE_FORMAT(u.birth,'%m-%d'), 0 , -1 ) AS Edad, ap.date AS FechaConsulta, 
             ap.hour AS HoraConsulta, ap.diagnostics as Diagnóstico,  usu.firstname as Medico, ap.note as NotasMédicas, ap.cancel as CitaCancelada, p.pathologicalAntecedents as Antecedentespatológicos,
             p.treatmentsReceived as TratamientosRecibidos, p.medicalObservations as ObservacionesMédicas, p.gestationWeeks as SemanasGestación, ap.reportOfFetalMovements as ReporteMovimientoFetales, 
             ap.arObro as ArOBro, ap.mainReasonForTheConsultation as RazónPrincipaldelaConsulta, ap.diagnostics as Diagnostico, ap.plans as Planes, ap.otherRemarks as OtrosComentarios
